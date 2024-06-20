ec2c5030d6cfd989042333e4609f31ea1def7db4
pdirafa <rafael.macieira@pditabira.com>
2024-06-20 16:06:15 -0300

    2.2

---
+++ Modulo 2/aula2_questao2.py
@@ -0,0 +1,5 @@
+tx = "o resultado é "
+n1 = 10
+n2 = 3.5
+n3 = n1+n2
+print(tx, type(tx), n1, type(n1), n2, type(n2), n3, type(n3))
---
+++ Modulo 2/aula2_questao4.py
@@ -0,0 +1,3 @@
+saldo = 500
+juros = 1.01
+print("após 3 meses meu novo saldo é ", (saldo*juros**3))
